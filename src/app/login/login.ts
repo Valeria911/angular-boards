04974cc14b76432469c0d09a661b2497ecfa3c9e
Valeria Gutiérrez Aravena <valegutierrezaravena@gmail.com>
2025-11-30 22:30:36 -0300

    login component

---
+++ src/app/login/login.ts
@@ -0,0 +1,61 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from '../services/auth.service';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.html',
+  styleUrls: ['./login.css']
+})
+export class LoginComponent {
+
+  formLogin!: FormGroup;
+
+  error: boolean = false;
+
+  constructor(
+    private auth: AuthService,
+    private router: Router,
+    private fb: FormBuilder
+  ) {
+    this.formLogin = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required]]
+    });
+  }
+
+  login() {
+    this.error = false;
+    if (this.formLogin.invalid) {
+      this.formLogin.markAllAsTouched();
+      return;
+    }
+
+    const { email, password } = this.formLogin.value;
+    const usuarios = JSON.parse(localStorage.getItem('usuarios') || '[]');
+    const usuario = usuarios.find((u: any) =>
+      u.email === email && u.password === password
+    );
+
+    if (usuario) {
+      localStorage.setItem('sesion', JSON.stringify({
+        logueado: true,
+        usuario: usuario.usuario,
+        tipo: usuario.tipo || 'cliente'
+      }));
+
+      //para test
+      window.location.assign(usuario.tipo === 'admin' ? '/admin' : '/perfil');
+
+    } else {
+      this.error = true;
+    }
+  }
+
+  limpiar() {
+    this.formLogin.reset();
+    this.error = false;
+  }
+}
+
