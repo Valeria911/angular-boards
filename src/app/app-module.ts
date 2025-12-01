c02dbf6f8cac119514dff57ad3f4b6b97b291238
Valeria Gutiérrez Aravena <valegutierrezaravena@gmail.com>
2025-11-30 22:33:59 -0300

    App

--- src/app/app-module.ts
+++ src/app/app-module.ts
@@ -1,16 +1,39 @@
 import { NgModule, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing-module';
 import { App } from './app';
+import { HomeComponent } from './home/home';
+import { AdminComponent } from './admin/admin';
+import { LoginComponent } from './login/login';
+import { PerfilComponent } from './perfil/perfil';
+import { RegistroComponent } from './registro/registro';
+import { RecuperarPasswordComponent } from './recuperar-password/recuperar-password';
+import { CarritoComponent } from './carrito/carrito';
+import { CategoriaComponent } from './categoria/categoria';
+import { NavbarComponent } from './navbar/navbar';
+import { FooterComponent } from './footer/footer';
 
 @NgModule({
   declarations: [
-    App
+    App,
+    HomeComponent,
+    AdminComponent,
+    LoginComponent,
+    PerfilComponent,
+    RegistroComponent,
+    RecuperarPasswordComponent,
+    CarritoComponent,
+    CategoriaComponent,
+    NavbarComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [
     provideBrowserGlobalErrorListeners(),
--- src/app/app-routing-module.ts
+++ src/app/app-routing-module.ts
@@ -1,10 +1,29 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home';
+import { AdminComponent } from './admin/admin';
+import { CarritoComponent } from './carrito/carrito';
+import { CategoriaComponent } from './categoria/categoria';
+import { LoginComponent } from './login/login';
+import { PerfilComponent } from './perfil/perfil';
+import { RecuperarPassword } from './recuperar-password/recuperar-password';
+import { Registro } from './registro/registro';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'home', component: HomeComponent },
+  { path: 'admin', component: AdminComponent },
+  { path: 'carrito', component: CarritoComponent },
+  { path: 'categoria/:id', component: CategoriaComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'perfil', component: PerfilComponent },
+  { path: 'recuperar-password', component: RecuperarPasswordComponent },
+  { path: 'registro', component: RegistroComponent }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes), FormsModule, ReactiveFormsModule],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
