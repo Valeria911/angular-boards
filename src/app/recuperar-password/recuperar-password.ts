72af34406a040fe860ce47613dcef090bbcb2b37
Valeria Gutiérrez Aravena <valegutierrezaravena@gmail.com>
2025-11-30 22:31:00 -0300

    recuperar component

---
+++ src/app/recuperar-password/recuperar-password.ts
@@ -0,0 +1,49 @@
+import { Component } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-recover',
+  templateUrl: './recuperar-password.html',
+  styleUrls: ['./recuperar-password.css'],
+  imports: [ReactiveFormsModule]
+})
+export class RecuperarPasswordComponent {
+
+  formRecover!: FormGroup;
+  mensaje = '';
+  error = '';
+
+  constructor(private fb: FormBuilder) {
+    this.formRecover = this.fb.group({
+      email: ['', [Validators.required, Validators.email]]
+    });
+  }
+
+  recuperar() {
+    this.mensaje = '';
+    this.error = '';
+
+    // valida formulario
+    if (this.formRecover.invalid) {
+      this.formRecover.markAllAsTouched();
+      return;
+    }
+
+    const email = this.formRecover.value.email;
+    const usuarios = JSON.parse(localStorage.getItem('usuarios') || '[]');
+    const usuario = usuarios.find((u: any) => u.email === email);
+
+    if (!usuario) {
+      this.error = 'No se encuentra cliente con ese correo';
+    } else {
+      this.mensaje = `Hola ${usuario.nombre}, tu contraseña es: ${usuario.password}`;
+    }
+  }
+
+  limpiar() {
+    this.formRecover.reset();
+    this.error = '';
+    this.mensaje = '';
+  }
+}
